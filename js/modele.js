5188728a2cc4e5bda1f53353494e1a654563ca2f
muhamedhdz <muhamedhodzic8@hotmail.com>
2023-03-14 11:05:03 +0100

    test à la main

--- js/modele.js
+++ js/modele.js
@@ -2,9 +2,6 @@
 
 //toutes les méthodes de la classe Modele
 
-
-//l'appel à l'API
-
 export {APP_ID, APP_KEY};
 
 class Modele{
@@ -15,14 +12,14 @@ class Modele{
 
 
     async fetchAPI() {
-        const baseURL = `https://api.edamam.com/search?q=${this.searchText}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=15`;
+       // const baseURL = `https://api.edamam.com/search?q=${this.searchText}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=15`;
+        const baseURL = `https://api.edamam.com/search?q=chicken&app_id=22297be0&app_key=9b01c1e1d8cf19df33ed768b6c8f0b64&from=0&to=15`;
         console.log(this.searchText);
         //on va récupérer les données de l'API
         const response = await fetch(baseURL);
         //on va convertir les données en JSON
         const data = await response.json();
-        console.log("test");
-    
+
         try {
             // Ouverture du fichier en mode écriture
             $file = fopen('../api/datarep.json', 'w');
