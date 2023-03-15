2d65a93ac004986fa499e34b3555f89e51819c89
muhamedhdz <muhamedhodzic8@hotmail.com>
2023-03-15 13:45:43 +0100

    fix response fetch

--- js/controller.js
+++ js/controller.js
@@ -1,19 +1,10 @@
-
-/*
-import { APP_ID, APP_KEY } from 'config.js';
-*/
-
-
-// Path: js/controller.js
-
+//import { Modele } from 'modele.js';
 let modele = new Modele();
 
 /*
 Récupère la valeur de l'input (recherche) et l'envoie à l'API
 */
-view.research.addEventListener("click", () => {
-    console.log("SLT");
-    console.log(s);
+view.research.addEventListener("click", function () {
     search = document.querySelector("#RechercheForm").value;
     console.log(search);
     modele.setSearchedText(search); // On envoie la valeur de l'input à la classe Modele
--- js/modele.js
+++ js/modele.js
@@ -1,10 +1,12 @@
-
-
-//toutes les méthodes de la classe Modele
-
-export {APP_ID, APP_KEY};
+const APP_ID = '22297be0';
+const APP_KEY = '9b01c1e1d8cf19df33ed768b6c8f0b64';
 
 class Modele{
+
+    /*
+    *  @var string
+    */
+    searchText;
 
     constructor(){
         this.searchText = '';
@@ -20,26 +22,15 @@ class Modele{
 
 
     async fetchAPI() {
-        const baseURL = `https://api.edamam.com/search?q=${this.searchText}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=15`;
-    //    const baseURL = `https://api.edamam.com/search?q=chicken&app_id=22297be0&app_key=9b01c1e1d8cf19df33ed768b6c8f0b64&from=0&to=15`;
-    //    console.log(this.searchText);
+        const baseURL = `https://api.edamam.com/search?q=${this.searchText}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=15`;
         //on va récupérer les données de l'API
         const response = await fetch(baseURL);
         //on va convertir les données en JSON
-        const data = await response.json();
-
-        try {
-            // Ouverture du fichier en mode écriture
-            $file = fopen('datarep.json', 'w');
-            // Écriture de la chaine récupérée dans le fichier etat.json
-            fputs($file, $data);
-            // Fermeture du fichier
-            fclose($file);
-          } catch (Exception) {
-            // Définition du statut de la réponse puis levée d'une exception
-            throw new Exception("Erreur " + Exception);
-          }
-    
+         const data = await response.json();
+        //afficher pr voir
+        console.log(data);
+        //console.log(JSON.stringify(data));
+        return data;
     }
 
 }
