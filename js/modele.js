49a979073293387f01906b9829ce20acfb835d50
muhamedhdz <muhamedhodzic8@hotmail.com>
2023-03-15 15:03:33 +0100

    début html intégration

--- js/modele.js
+++ js/modele.js
@@ -22,15 +22,38 @@ class Modele{
 
 
     async fetchAPI() {
-        const baseURL = `https://api.edamam.com/search?q=${this.searchText}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=15`;
+        const baseURL = `https://api.edamam.com/search?q=${this.searchText}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3`;
         //on va récupérer les données de l'API
         const response = await fetch(baseURL);
         //on va convertir les données en JSON
-         const data = await response.json();
+        const data = await response.json();
         //afficher pr voir
         console.log(data);
         //console.log(JSON.stringify(data));
+        this.afficher(data);
         return data;
     }
+    
+    //on va créer une fonction qui va afficher les données de l'API dans l'HTML
+
+    afficher(data){
+        for(let i = 0; i < data.hits.length; i++){
+            const res = document.createElement("p"); 
+            res.classList.add("res");
+            res.innerText = data.hits[i].recipe.label;  
+            view.blocresultat.appendChild(res);
+        }
+    }
+
+
+    
 
 }
+
+// on veut afficher
+
+// 1. Image (hits.recipe.image)
+// 2. Label (hits.recipe.label)
+
+//étape 2
+// 3. Ingredients (hits.recipe.ingredientLines)
--- js/vue.js
+++ js/vue.js
@@ -6,6 +6,19 @@ const view = {
    
    research: document.getElementById("btn-lancer-recherche"),
 
+   resultat: document.querySelectorAll(".res"),
 
+   blocresultat: document.getElementById('bloc-resultats'),
 }
 
+
+/*
+  const res = document.querySelector(".res");
+            const url = "https://api.edamam.com/search?q=chicken&app_id=1f1d8b1f&app_key=1f1d8b1f";
+            fetch(url)
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(data);
+                res.innerHTML = data.hits[0].recipe.label;
+              });
+*/
