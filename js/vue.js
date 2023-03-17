788cb3fd91ce3ae2bcda469889871f965a784b1e
hodzicm <muhamed.hodzic@etu.univ-grenoble-alpes.fr>
2023-03-17 04:23:32 +0100

    fav / début localstorage

--- js/vue.js
+++ js/vue.js
@@ -15,17 +15,8 @@ const view = {
 
    sectionfavoris: document.getElementById("section-favoris"),
 
+   etoile: document.getElementById("etoile"),
+
+   btn_delete: document.querySelectorAll(".btn_delete"),
 
 }
-
-
-/*
-  const res = document.querySelector(".res");
-            const url = "https://api.edamam.com/search?q=chicken&app_id=1f1d8b1f&app_key=1f1d8b1f";
-            fetch(url)
-              .then((response) => response.json())
-              .then((data) => {
-                console.log(data);
-                res.innerHTML = data.hits[0].recipe.label;
-              });
-*/
